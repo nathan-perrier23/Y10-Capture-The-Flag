54f7e6a9a232be522f59d6df7da3b4eee58419cd
nathan-perrier23 <nathanperrier138@gmail.com>
2024-02-28 18:03:27 +1000

    Delete unused images and files

--- GameFrame/Flag.py
+++
@@ -1,9 +0,0 @@
-from GameFrame import RoomObject, Globals
-
-
-class Flag(RoomObject):
-    def __init__(self, room, x, y, image_file_name):
-        RoomObject.__init__(self, room, x, y)
-
-        flag_image = self.load_image(image_file_name)
-        self.set_image(flag_image, 32, 32)
